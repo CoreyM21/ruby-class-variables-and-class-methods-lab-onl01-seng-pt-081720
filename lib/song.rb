e49d6b98bc101fb2fe83de6d0e374e89b28356f7
Corey Martin <coreyjmartin2@gmail.com>
2020-09-09 18:15:30 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+require 'pry' 
+
+class Song 
+  
+  attr_accessor :name, :artist, :genre
+  
+  
+  @@count = 0 
+  @@artists = []
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @@count += 1
+     
+    @name = name
+    @artist = artist 
+    @genre = genre
+    
+    
+    @@artists << artist
+    @@genres << genre 
+    
+    
+  end
+  
+  
+  def self.count
+    
+    @@count
+  end
+  
+  def self.artists
+    @@artists.uniq 
+  end
+    
+    def self.genres 
+      @@genres.uniq 
+    end
+    
+    def self.genre_count
+      hash = {}
+      @@genres.each do |g|
+        
+       if hash[g]
+         hash[g] += 1
+       else
+          hash[g] = 1 
+       end
+      end
+        hash 
+    end
+  
+  
+end
